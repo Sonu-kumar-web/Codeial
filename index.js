50f648b30ec35a3de8564eb0ad57ff8e6e3a8be5
Sonu <sonu3660@gmail.com>
2020-02-25 23:31:05 +0530

    complete to create view for home

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 8000;
 // Use express router
 app.use('/',require('./routes'));
 
+// Set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running tha server: ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,10 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 console.log('Router Loaded');
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
+
 module.exports=router;
