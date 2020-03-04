d84f27ef02eacd38db4e484f52a5d47c1a60dca6
Sonu <sonu3660@gmail.com>
2020-03-04 10:04:56 +0530

    add layouts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile=function(req,res){
-    res.end('<h1>User Profile</h1>');
+    // res.end('<h1>User Profile</h1>');        send direct message to the server
+    return res.render('users_profile',{       // Render views/users_profile file to the server
+        title: "profile"
+    });
 }
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+
+// include layouts library
+const expressLayouts=require('express-ejs-layouts');
+app.use(expressLayouts);
+
 // Use express router
 app.use('/',require('./routes'));
 
