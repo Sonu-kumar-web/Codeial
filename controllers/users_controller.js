822ba2688e60515c4f9272fcf63b1f716ea2325b
Sonu <sonu3660@gmail.com>
2020-03-06 15:07:57 +0530

    add manula authen.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,9 +4,21 @@ const User=require('../models/user');
 // Render the profile page
 module.exports.profile=function(req,res){
     // res.end('<h1>User Profile</h1>');        send direct message to the server
-    return res.render('users_profile',{       // Render views/users_profile file to the server
-        title: "profile"
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+            if(user){
+                return res.render('users_profile',{
+                    title: "User Profile",
+                    user: user
+                });
+                
+            }else{
+                return res.redirect('/users/sign-in');
+            }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 // Render the Sign-up page
@@ -55,5 +67,25 @@ module.exports.create=function(req,res){
 
 // Sign in and create a session for user
 module.exports.createSession=function(req,res){
-    // Todo later
+    // Steps to Authenticate
+        // Find the user
+        User.findOne({email: req.body.email},function(err,user){
+            if(err){
+                console.log('Error in finding user in signing in');
+                return;
+            }
+            // Handle user found
+            if(user){
+                // Handle password which doesn't match 
+                if(user.password != req.body.password){
+                    return res.redirect('back');
+                }
+                // Handle session creation
+                res.cookie('user_id',user.id);
+                return res.redirect('/users/profile');
+            }else{
+                // Handle user not found
+                return res.redirect('back');
+            }
+        });
 }
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ const express=require('express');
 const router=express.Router();
 const usersController=require('../controllers/users_controller');
 router.get('/profile', usersController.profile);
+
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);          // sign-in in if account is present
+
 router.post('/create',usersController.create);          // create now account 
+router.post('/create-session',usersController.createSession);       // Create session when user sign-in
+
 module.exports=router;
