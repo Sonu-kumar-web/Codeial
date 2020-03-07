7a83e2f70a05bfef0b5a5882341bd7a60820a577
Sonu <sonu3660@gmail.com>
2020-03-08 00:38:28 +0530

    add sign out link

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -63,3 +63,9 @@ module.exports.create=function(req,res){
 module.exports.createSession=function(req,res){
     return res.redirect('/');
 }
+
+// Sign Out and destroy the current session
+module.exports.destroySession=function(req,res){
+    req.logout();
+    return res.redirect('/');
+}
