7cc09f6affadeddc836d9c84165bccf69d100495
Sonu <sonu3660@gmail.com>
2020-03-07 20:17:31 +0530

    Setting current authentication user

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -38,4 +38,22 @@ passport.deserializeUser(function(id, done){
     });
 });
 
+// Check if the user is Authenticated
+passport.checkAuthentication=function(req, res, next){
+    // if the user is sign-in, the pass on the request to the next function (controller's action)
+    if(req.isAuthenticated()){
+        return next();
+    }
+    // if the user is not sign-in
+    return res.redirect('/user/sign-in');
+}
+
+passport.setAuthenticatedUser=function(req, res, next){
+    if(req.isAuthenticated()){
+        // req.user contains the current signed-in user from the session cookie and we are just sending this to the locals for the views
+        res.locals.user=req.user;
+    }
+    next();
+} 
+
 module.exports=passport;
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ const express=require('express');
 const router=express.Router();
 const passport=require('passport');
 const usersController=require('../controllers/users_controller');
-router.get('/profile', usersController.profile);
+// router.get('/profile', usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);          // sign-in in if account is present
 router.post('/create',usersController.create);          // create now account 
