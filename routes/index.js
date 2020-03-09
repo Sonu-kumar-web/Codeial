9ba6d9327a8fed11d5bc37c31a9b574f290718e6
Sonu <sonu3660@gmail.com>
2020-03-09 23:29:30 +0530

    Saving posts to the Database

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const homeController=require('../controllers/home_controller');
 console.log('Router Loaded');
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
