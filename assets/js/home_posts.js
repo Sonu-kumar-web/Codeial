3e65da5ede24b877f4b5d194fa4f8696e44c02ba
Sonu <sonu3660@gmail.com>
2020-03-12 22:58:44 +0530

    Creating a post: sending data

---
+++ assets/js/home_posts.js
@@ -0,0 +1,20 @@
+{
+    // Method to submit the form data for new post using ajax
+    let createPost=function(){
+        let newPostForm=$('#new-post-form');     //Form's id
+        newPostForm.submit(function(e){
+            e.preventDefault();
+            $.ajax({
+                type: 'post',
+                url: 'posts/create',
+                data: newPostForm.serialize(),
+                success: function(data){
+                    console.log(data);
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            })
+        });
+    }
+    createPost();
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -18,10 +18,22 @@ const Comment=require('../models/comment');
 // Create a post by Async Await
 module.exports.create= async function(req, res){
     try {
-        await Post.create({
+        // await Post.create({
+        let post=await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        // if the data in ajax
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post Created"
+            });
+        }
+
         req.flash('success','Post Published!');
         return res.redirect('back');
 
