e9a08f25776d8fac7192bc3a072e639d3c88faf7
Sonu <sonu3660@gmail.com>
2020-03-12 10:36:04 +0530

    add Async await and error handling

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post=require('../models/post');
 const User=require('../models/user');
 
-module.exports.home=function(req,res){
+// module.exports.home=function(req,res){
 
     // return res.end('<h1>Express is up for codeial</h1>');   serve directly to the browser
     
@@ -19,14 +19,14 @@ module.exports.home=function(req,res){
     // });
 
     // Populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
+    // Post.find({})
+    // .populate('user')
+    // .populate({
+    //     path: 'comments',
+    //     populate: {
+    //         path: 'user'
+    //     }
+    // })
     // .exec(function(err,posts){
     //         return res.render('home',{
     //             title: "Codeial | Home",
@@ -35,15 +35,42 @@ module.exports.home=function(req,res){
     //     });
 
     // for show user on home page
-    .exec(function(err,posts){
-        User.find({},function(err, users){
-            return res.render('home',{
-                title: "Codeial | Home",
-                posts: posts,
-                all_users: users
-            });
-        })
+//     .exec(function(err,posts){
+//         User.find({},function(err, users){
+//             return res.render('home',{
+//                 title: "Codeial | Home",
+//                 posts: posts,
+//                 all_users: users
+//             });
+//         })
             
-    });
-}
+//     });
+// }
 
+// Convert code into async await
+module.exports.home= async function(req,res){
+    try {
+         // Populate the user of each post
+        let posts=await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+
+        // for show user on home page
+        let users=await User.find({});
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
+        });
+
+    } catch (err) {
+        console.log('Error', err);
+        return;
+        
+    }
+}
