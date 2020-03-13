67f1a237c9489972e6d4ecb351ff47962995c700
Sonu <sonu3660@gmail.com>
2020-03-13 11:24:44 +0530

    Setting up the directory structure

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
 
+router.use('/api',require('./api'));    // Set api path from router
+
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
