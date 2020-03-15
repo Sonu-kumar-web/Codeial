69af5812ac2c7307ab7c68dfde0a1a51cbb38bfc
Sonu <sonu3660@gmail.com>
2020-03-16 02:28:01 +0530

    Authentication and Authorization in APIs

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -10,7 +10,9 @@ module.exports.createSession = async function(req, res){
         }
         return res.json(200, {
             message: "Sign in successful, here is your token please keep it safe",
-            data: {token: jwt.sign(user.toJSON(),'codeial',{expiresIn: '10000'})}
+            data: {
+                token: jwt.sign(user.toJSON(),'codeial',{expiresIn: '1000000000000000'})
+            }
         });
     } catch (error) {
         console.log('Error',error);
