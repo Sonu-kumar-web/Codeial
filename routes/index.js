18af8fd93155ced5bfae8cc51daedb4c313bb27c
Sonu <sonu3660@gmail.com>
2020-03-18 12:21:42 +0530

    Created Actions and Routes for likes

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/',homeController.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
+router.use('/likes', require('./likes'));
 
 router.use('/api',require('./api'));    // Set api path from router
 // for any further routes, access from here
