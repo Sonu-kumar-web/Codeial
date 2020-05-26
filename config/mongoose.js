16d008b77600e10b18cf32f4dc115206580100e2
Sonu <sonu3660@gmail.com>
2020-05-26 21:45:56 +0530

    Configure settion to deploying on heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose');
 mongoose.connect('mongodb://localhost/codeial_development');
 const db=mongoose.connection;
-db.on('error',console.error.bind(console,"Error Connection to MongoDB"));
+// db.on('error',console.error.bind(console,"Error Connection to MongoDB"));
+mongoose.connect('mongodb+srv://sonu:sonu@free-cluster-t4e4s.mongodb.net/Todo?retryWrites=true&w=majority');
 db.once('open',function(){
     console.log('Connected to Database :: MongoBD');
 });
